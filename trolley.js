6fac2ff33c8f4b4fd6afa3375da285121b40f7f6
debarghyaDas6july2007 <91417563+debarghyaDas6july2007@users.noreply.github.com>
2022-01-20 10:47:45 +0530

    Add files via upload

---
+++ trolley.js
@@ -0,0 +1,26 @@
+class trolley {
+ 
+  constructor(x,y,w,h)
+  { var option ={
+      isStatic:true
+  }
+    this.x=x;
+    this.y=y;
+    this.w=w;
+    this.h=h;
+
+    this.image = loadImage("t-removebg-preview.png");
+    this.body=Bodies.rectangle(this.x,this.y,this.w,this.h,option);
+    World.add(world, this.body);
+  }
+  display()
+  {
+  var pos = this.body.position;
+  push()
+  translate(pos.x,pos.y)
+  fill(189)
+  imageMode(CENTER)
+  image(this.image,400,400,this.w,this.h)
+  pop()
+  }
+}
